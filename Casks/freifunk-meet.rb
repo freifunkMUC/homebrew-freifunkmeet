2b9e4fcd229ab2d01bfab737a9d2a4cc8b15e81a
Matthias Kesler <krombel@krombel.de>
2024-04-05 11:18:37 +0200

    extend list of trash files

--- Casks/freifunk-meet.rb
+++ Casks/freifunk-meet.rb
@@ -16,7 +16,13 @@ cask "freifunk-meet" do
   app "FreifunkMeet.app"
 
   zap trash: [
+                "~/Library/Application Support/Caches/ffmuc-meet-updater",
                 "~/Library/Application Support/FreifunkMeet",
                 "~/Library/Logs/FreifunkMeet",
+                "~/Library/Caches/meet.ffmuc.net",
+                "~/Library/Caches/meet.ffmuc.net.ShipIt",
+                "~/Library/Preferences/meet.ffmuc.net.plist",
+                "~/Library/Preferences/meet.ffmuc.net.helper.Plugin.plist",
+                "~/Library/Saved Application State/meet.ffmuc.net.savedState",
              ]
 end
